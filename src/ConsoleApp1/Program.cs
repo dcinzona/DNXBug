a1f3c6bc96c5d8ccb5730bb144b96540736c49c8
dcinzona <gmt@gmtaz.com>
2015-10-16 07:48:54 -0400

    Adding solution

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
